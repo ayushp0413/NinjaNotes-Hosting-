bcc854b6334023e7d6d938b7a65412a09c38bdf5
ayush patidar <ayushpatidar7441@gmail.com>
2024-07-31 18:23:41 +0530

    Till Our services

---
+++ src/assets/data/serviceCardData.js
@@ -0,0 +1,28 @@
+import { MdAutoStories } from "react-icons/md";
+import { MdArticle } from "react-icons/md";
+import { GiPapers } from "react-icons/gi";
+import { GiNewspaper } from "react-icons/gi";
+export const services = [
+    {
+        id:1,
+        icon: MdAutoStories,
+        heading: "University Notes",
+        description: "Access a wide variety of university notes covering various subjects and topics.",
+        btnText: "Learn More",
+    },
+    {
+        id:2,
+        icon: GiNewspaper,
+        heading: "Last Year Papers",
+        description: "Explore a wide variety of PYQ's covering all syllabus and important topics.",
+        btnText: "Learn More",
+    },
+    {
+        id:3,
+        icon: MdArticle,
+        heading: "Related Blogs",
+        description: "Read the educational blogs to strengthen your concepts and understanding.",
+        btnText: "Learn More",
+    }
+  ];
+  
---
+++ src/components/core/Home/ServiceCard.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+
+const ServiceCard = ({icon: Icon, heading, description, btnText}) => {
+
+  return (
+    <>
+        <div className='relative bg-black w-[300px] h-[400px] rounded-[30px]'>
+            <div className='absolute w-[300px] h-[400px] rounded-[30px] bg-[#7B9367] left-[6px] bottom-[6px] hover:left-0 hover:bottom-0 transition-all duration-200 text-white flex flex-col items-center justify-center p-[1rem] py-5'>
+                <Icon size={100} color="#f3f3f3" />
+                <div className='flex flex-col justify-center items-center mt-4'>
+                    <h3 className='font-groastek text-[32px]'>{heading}</h3>
+                    <p className='font-groastek font-light text-center -mt-3'>{description}</p>
+                </div>
+            
+                <button className='bg-black py-4 px-20 rounded-full text-xl'>{btnText}</button>
+            </div>
+        </div>
+         
+    </>
+  )
+}
+
+export default ServiceCard
