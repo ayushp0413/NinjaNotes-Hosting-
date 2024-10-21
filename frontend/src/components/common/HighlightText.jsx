a7030b30e9b9f10fa7741d65f7327cb83b534c23
ayush patidar <ayushpatidar7441@gmail.com>
2024-10-22 01:33:00 +0530

    Ready for deployment

--- frontend/src/components/common/HighlightText.jsx
+++ frontend/src/components/common/HighlightText.jsx
@@ -2,10 +2,8 @@ import React from 'react'
 import "../../index.css"
 
 const HighlightText = ({text, className}) => {
-
-  console.log("Classname : ", className);
   return (
-        <span className={`highlightText ${className}  `}>{ text}</span>
+        <span className={`highlightText ${className} max-w-[100px] `}>{ text}</span>
   )
 }
 
